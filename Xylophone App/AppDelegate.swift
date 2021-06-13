3595e9190e339e9323867dd06d4b9e556050038a
Nishant Kumar <nishantcafe@gmail.com>
2021-06-14 00:27:20 +0530

    Adding Redme

--- Xylophone App/AppDelegate.swift
+++ Xylophone App/AppDelegate.swift
@@ -1,13 +1,14 @@
 //
 //  AppDelegate.swift
-//  Xylophone App
+//  Xylophone
 //
-//  Created by Satyam Kumar on 14/06/21.
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -15,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
     // MARK: UISceneSession Lifecycle
--- Xylophone App/ViewController.swift
+++ Xylophone App/ViewController.swift
@@ -1,19 +1,39 @@
 //
 //  ViewController.swift
-//  Xylophone App
+//  Xylophone
 //
-//  Created by Satyam Kumar on 14/06/21.
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(sound: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    //Bring's sender's opacity back up to fully opaque.
+                    sender.alpha = 1.0
+                }
+        
+    }
+    
+    func playSound(sound : String) {
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
